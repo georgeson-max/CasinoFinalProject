bc3f1c84ce796cbf9e1c82663f06250061643e5d
georgeson-max <mgeorgeson@nevada.unr.edu>
2022-04-25 10:58:15 -0700

    Changed assertequals to assertequal and renamed action

--- test_main.py
+++ test_main.py
@@ -5,23 +5,23 @@ from classes import *
 class TestGame(unittest.TestCase):
     def test_person_init(self):
         p = Person()
-        self.assertEquals(p.bank, 500)
-        self.assertEquals(p.current_bet, 0)
-        self.assertEquals(p.num_choice, 40)
-        self.assertEquals(p.choice, '')
-        self.assertEquals(p.odds, 0)
+        self.assertEqual(p.bank, 500)
+        self.assertEqual(p.current_bet, 0)
+        self.assertEqual(p.num_choice, 40)
+        self.assertEqual(p.choice, '')
+        self.assertEqual(p.odds, 0)
 
     def test_roulette_init(self):
         r = Roulette()
-        self.assertEquals(len(r.numbers),37)
-        self.assertEquals(len(r.colors),37)
-        self.assertEquals(r.number,0)
-        self.assertEquals(r.min,1)
-        self.assertEquals(r.max,1000)
-        self.assertEquals(r.color,'')
-        self.assertEquals(r.dozen,'')
-        self.assertEquals(r.even_odd,'')
-        self.assertEquals(r.low_high,'')
+        self.assertEqual(len(r.numbers),37)
+        self.assertEqual(len(r.colors),37)
+        self.assertEqual(r.number,0)
+        self.assertEqual(r.min,1)
+        self.assertEqual(r.max,1000)
+        self.assertEqual(r.color,'')
+        self.assertEqual(r.dozen,'')
+        self.assertEqual(r.even_odd,'')
+        self.assertEqual(r.low_high,'')
 
 
 if __name__ == '__main__':
